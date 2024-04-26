b292204efb51ace70f8e9f4a418cfee8023951f2
esmanur-uzun <esmabitmez@gmail.com>
2024-04-26 09:50:59 +0300

    multiple file upload completed successfully

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ export const app = Fastify({
 
 app.register(multipart);
 
+// tekli dosya yükleme
+
 app.post("/upload", async (req, reply) => {
   const data = await req.file();
   if (!data) {
     reply.code(400).send({ message: "Dosya bulunamadı." });
     return;
   }
-  const targetPath = `./uploads/${data.filename}`;
 
   try {
     await pump(data.file,fs.createWriteStream(`./uploads/${data.filename}`));
@@ -34,16 +35,40 @@ app.post("/upload", async (req, reply) => {
   return { message: "dosya yüklendi" };
 });
 
+// çoklu doysa yükleme
+
+// app.post("/upload", async (req, reply) => {
+//   const parts = req.files();
+//   if (!parts) {
+//     reply.code(400).send({ message: "Dosyalar bulunamadı" });
+//     return;
+//   }
+//   try {
+//     const uploadedFiles = [];
+//     for await (const part of parts) {
+      
+//       await pump(part.file, fs.createWriteStream(`./uploads/${part.filename}`));
+//       const fileUrl = `${req.protocol}://${req.hostname}/uploads/${part.filename}`;
+//       uploadedFiles.push({ filename: part.filename, url: fileUrl });
+      
+//     }
+//     return { message: "Dosyalar başarıyla yüklendi ve kaydedildi",uploadedFiles };
+//   } catch (err) {
+//     console.error("Dosya yüklenirken bir hata oluştu: ",err)
+//     reply.code(500).send({message:"Dosya kaydedilirken bir hata oluştu"})
+//   }
+//   return { message: "yükleme başarılı" };
+// });
+
 // Upload edilen dosyaların dışarıya erişilebilir hale getirilmesi
 app.register(fastifyStatic, {
-  root: 'C:\\Users\\pc\\Desktop\\fastify\\uploads', // Dosyaların bulunduğu dizin
-  prefix: '/uploads' // Dışarıya erişim için kullanılacak URL öneki
+  root: "C:\\Users\\pc\\Desktop\\fastify\\uploads", // Dosyaların bulunduğu dizin
+  prefix: "/uploads", // Dışarıya erişim için kullanılacak URL öneki
 });
-
 
 const start = async () => {
   try {
-    await app.listen({ port }); // Değişiklik: Listen yöntemi değiştirildi
+    await app.listen({ port });
     console.log("Server bağlantısı başarılı. Port:", port);
   } catch (err) {
     console.error("Server bağlantısı başarısız:", err);
